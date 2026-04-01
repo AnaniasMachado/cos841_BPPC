5b8104421ad11747d1a2bbbd712231042490723b
Ananias <86498679+AnaniasMachado@users.noreply.github.com>
2026-04-01 17:00:32 -0300

    Removing old code that didn't use delta updates.

--- src/util/local_search.cpp
+++ src/util/local_search.cpp
@@ -10,42 +10,6 @@ void LocalSearch::setSolution(BPPCSolution& solution) {
     sol = &solution;
 }
 
-// // -------------------- Relocation --------------------
-// bool LocalSearch::relocation() {
-//     int best_obj = computeObjective(*sol);
-//     int best_from=-1, best_to=-1, best_item=-1;
-
-//     for (size_t from = 0; from < sol->bins.size(); from++) {
-//         for (size_t i = 0; i < sol->bins[from].size(); i++) {
-//             int item = sol->bins[from][i];
-
-//             for (size_t to = 0; to <= sol->bins.size(); to++) {
-//                 if (to == from) continue;
-
-//                 BPPCSolution temp = *sol;
-//                 temp.moveItem(item, from, to);
-
-//                 int obj = computeObjective(temp);
-
-//                 if (obj < best_obj) {
-//                     best_obj = obj;
-//                     best_from = from;
-//                     best_to = to;
-//                     best_item = item;
-//                 }
-//             }
-//         }
-//     }
-
-//     if (best_item != -1) {
-//         sol->moveItem(best_item, best_from, best_to);
-//         sol->removeEmptyBins();
-//         return true;
-//     }
-
-//     return false;
-// }
-
 // -------------------- Relocation (delta version) --------------------
 bool LocalSearch::relocation() {
     int current_obj = computeObjective(*sol);
@@ -80,40 +44,6 @@ bool LocalSearch::relocation() {
 
     return false;
 }
-
-// // -------------------- Exchange --------------------
-// bool LocalSearch::exchange() {
-//     int best_obj = computeObjective(*sol);
-//     int b1_best=-1, b2_best=-1, i_best=-1, j_best=-1;
-
-//     for (size_t b1 = 0; b1 < sol->bins.size(); b1++) {
-//         for (size_t i = 0; i < sol->bins[b1].size(); i++) {
-//             for (size_t b2 = b1+1; b2 < sol->bins.size(); b2++) {
-//                 for (size_t j = 0; j < sol->bins[b2].size(); j++) {
-
-//                     BPPCSolution temp = *sol;
-//                     temp.swapItems(b1,i,b2,j);
-
-//                     int obj = computeObjective(temp);
-
-//                     if (obj < best_obj) {
-//                         best_obj = obj;
-//                         b1_best=b1; b2_best=b2;
-//                         i_best=i; j_best=j;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-//     if (i_best != -1) {
-//         sol->swapItems(b1_best,i_best,b2_best,j_best);
-//         sol->removeEmptyBins();
-//         return true;
-//     }
-
-//     return false;
-// }
 
 // -------------------- Exchange (delta version) --------------------
 bool LocalSearch::exchange() {
@@ -150,37 +80,6 @@ bool LocalSearch::exchange() {
 
     return false;
 }
-
-// // -------------------- Add --------------------
-// bool LocalSearch::add() {
-//     int best_obj = computeObjective(*sol);
-//     int best_from=-1, best_item=-1;
-
-//     for (size_t b = 0; b < sol->bins.size(); b++) {
-//         for (size_t i = 0; i < sol->bins[b].size(); i++) {
-//             int item = sol->bins[b][i];
-
-//             BPPCSolution temp = *sol;
-//             temp.moveItem(item, b, sol->bins.size());
-
-//             int obj = computeObjective(temp);
-
-//             if (obj < best_obj) {
-//                 best_obj = obj;
-//                 best_from = b;
-//                 best_item = item;
-//             }
-//         }
-//     }
-
-//     if (best_item != -1) {
-//         sol->moveItem(best_item, best_from, sol->bins.size());
-//         sol->removeEmptyBins();
-//         return true;
-//     }
-
-//     return false;
-// }
 
 // -------------------- Add (delta version) --------------------
 bool LocalSearch::add() {
